a8dd49a6c602f02eee9d6eb391fa7d9218f25a35
erickt2006.git@gmail.com <erickt2006.git@gmail.com>
2026-03-24 12:59:48 -0500

    first commit

---
+++ src/main/java/ec/ucacue/registro/hombre_maquina/HombreMaquinaApplication.java
@@ -0,0 +1,13 @@
+package ec.ucacue.registro.hombre_maquina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HombreMaquinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HombreMaquinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/ucacue/registro/hombre_maquina/HombreMaquinaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.ucacue.registro.hombre_maquina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HombreMaquinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
